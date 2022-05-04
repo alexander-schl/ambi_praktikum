3e0f38c16782e4da0df84b27a7ca9c90a5fcebc8
Steinadler <alexander.schleiter@hotmail.de>
2022-05-04 19:36:39 +0200

    addded boyer_moore, compute_last_occurence, compute_good_suffix, compute_prefix. Error check slide 13 in boyer_moore lecture

--- praktikum1.py
+++ praktikum1.py
@@ -31,7 +31,7 @@ def menu():
             if number == 3:
                 knuth()
             if number == 4:
-                boyer()
+                boyer_moore_matcher()
             if number == 5:
                 exit()
             else:
@@ -108,10 +108,73 @@ def knuth():
     pass
 
 
-def boyer():
-    pass
+def compute_prefix(P):
+    m = len(P)
+    # initialize list of length m
+    pi = [0] * m
+    k = 0
+
+    for q in range(2, m):
+        while k > 0 and P[k + 1] != P[q]:
+            k = pi[k]
+
+        if P[k + 1] == P[q]:
+            k = k + 1
+        pi[q] = k
+
+    return pi
+
+
+def boyer_moore_matcher(T, P, sigma):
+    n = len(T)
+    m = len(P)
+    lambd = compute_last_occurrence(P, m, sigma)
+    gamma = compute_good_suffix(P, m)
+    o = 0
+    s = o
+
+    while s <= n - m:
+        j = m
+        while j > o and P[j] == T[s + j]:
+            j = j - 1
+            if j == o:
+                print("Pattern occurs with shift ", s)
+                s = s + gamma[o]
+            else:
+                s = s + max(gamma[j], j - lambd[T[s + j]])
+
+
+def compute_last_occurrence(P, m, sigma):
+    lambd = []
+
+    for char in sigma:
+        lambd.append(0)
+
+    for j in range(m):
+        lambd[P[j]] = j
+
+    return lambd
+
+
+def compute_good_suffix(P, m):
+    pi = compute_prefix(P)
+    P1 = reversed(P)
+    pi1 = compute_prefix(P1)
+    gamma = [0] * m
+
+    for j in range(m):
+        gamma[j] = m - pi[m]
+    for l in range(m):
+        j = m - pi1[l]
+        if gamma[j] > l - pi1[l]:
+            gamma[j] = l - pi1[l]
+
+    return gamma
 
 
 if __name__ == "__main__":
     rabin_karp_matcher("jengdjensjen", "jen")
+    unicode_char_list = [chr(x) for x in range(255)]
+    print(unicode_char_list)
+    boyer_moore_matcher("jengdjensjen", "jen", unicode_char_list)
     # menu()
