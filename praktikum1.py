aa19be2ec68d701e768e27117d1da198944563ea
Steinadler <alexander.schleiter@hotmail.de>
2022-05-04 15:49:37 +0200

    rabin karp with bug in last line

--- praktikum1.py
+++ praktikum1.py
@@ -2,11 +2,11 @@ def menu():
     print("AMBI Praktikum Aufgabe 1 von Alexander Schleiter & Tim Stadager")
     i = 0
     while i == 0:
-        print("Als erstes bitte ein Pattern als Datei pfad angeben"
-              "und dann den darin zu suchenden String")
+        print("Als erstes bitte einen Text als Dateipfad angeben"
+              "und dann den darin zu suchenden Pattern.")
         try:
-            pattern = str(input(r"Pattern:    "))
-            search_string = str(input(r"String:  "))
+            pattern = str(input(r"Text:    "))
+            search_string = str(input(r"Pattern:  "))
             string_input(pattern, search_string)
         except TypeError:
             print("Das Pattern konnte nicht gefunden werden.")
@@ -14,7 +14,7 @@ def menu():
         except FileNotFoundError:
             print("Der Eingegebene Pfad ist nicht vorhanden.")
             continue
-        print("Für die Verschiedenen Algorithmen drücken sie die 1-5")
+        print("Für die verschiedenen Algorithmen drücken Sie die 1-5:")
         print("1. Naiver Pattern Matcher\n"
               "2. Rabin-Karp\n"
               "3. Knuth-Morris-Pratt\n"
@@ -35,10 +35,10 @@ def menu():
             if number == 5:
                 exit()
             else:
-                print("Die Angegebene Zahl liegt nicht zwischen 1 und 5\n"
-                      "Bitte versuchen sie es erneut")
+                print("Die angegebene Zahl liegt nicht zwischen 1 und 5\n"
+                      "Bitte versuchen Sie es erneut.")
         except ValueError:
-            print("Geben sie eine Zahl zwischen 1 und 5 ein")
+            print("Geben Sie eine Zahl zwischen 1 und 5 ein.")
 
 
 def string_input(pattern, search_string, ):
@@ -46,7 +46,7 @@ def string_input(pattern, search_string, ):
     s = " "
     pattern.replace("\"", "/")
 
-    with open(pattern, "r") as text:
+    with open(pattern, "r", encoding="utf-8") as text:
         # content = text.read().replace("\n","")
         for i in text:
             if i[0] == ">":
@@ -79,18 +79,29 @@ def naive_string_matcher(t, p):
             print("Pattern occurs with shift", s)
 
     return shifts
+#144697
+#1000000009
 
-
-def rabin_karp_matcher(T, P, d=144697, q=999999999989):
+def rabin_karp_matcher(T, P, d=100, q=113):
     n = len(T)
     m = len(P)
     h = d ^ (m - 1) % q
     p = 0
     t0 = 0
 
-    for i in range(0, m+1):
-        p = (d*p+P[i]) % q
-        t0 = (d*t0 + T[i]) % q
+    for i in range(0, m):
+        p = (d * p + ord(u"{}".format(P[i]))) % q
+        t0 = (d * t0 + ord(u"{}".format(T[i]))) % q
+
+    for s in range(0, n - m + 1):
+        if p == t0:
+            if P[0:m] == T[s: s + m]:
+                print("Pattern occurs with shift", s)
+
+        if s < (n - m):
+            # t0 = (ord(u"{}".format(T[s + m])) + d * (t0 - ord(u"{}".format(T[s+1])) * h)) % q
+            t0 = (d * (t0 - ord(u"{}".format(T[s+1])) * h) + ord(u"{}".format(T[s + m]))) % q
+
 
 def knuth():
     pass
@@ -101,5 +112,6 @@ def boyer():
 
 
 if __name__ == "__main__":
-    naive_string_matcher("gdjensjen", "jen")
+    # naive_string_matcher("gdjensjen", "jen")
+    rabin_karp_matcher("jengdjensjen", "jen")
     # menu()
